e62621426e33601c672ad2afe64d0ec7e1c8092a
Dylan McAulay <dmcaulay97@gmail.com>
2022-07-27 12:17:51 -0400

    finished about page and added pacman loaders for home and about

---
+++ src/components/About/index.js
@@ -0,0 +1,72 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBootstrap, faHtml5, faCss3, faReact, faJsSquare, faGitAlt } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders';
+
+const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    }, [])
+
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15} />
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                    <p>
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faBootstrap} color='#563e7c' />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color='#f06529' />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color='#28A4d9' />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Loader type='pacman' />
+        </>
+    )
+}
+
+export default About;
