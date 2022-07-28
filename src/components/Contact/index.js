0ffcbc097f25c89a8a4f3afc498327f0bf8a6f1f
Dylan McAulay <dmcaulay97@gmail.com>
2022-07-28 14:51:42 -0400

    added contact page map element

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
 
@@ -89,6 +90,25 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Dylan McAulay,
+                    <br />
+                    United States,
+                    <br />
+                    Danbury, CT
+                    <br />
+                    <span>dmcaulay97@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[41.3948, -73.4540]} zoom={9}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[41.3948, -73.4540]}>
+                            <Popup>
+                                I am here!
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman' />
